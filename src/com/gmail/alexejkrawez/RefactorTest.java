752d57b62b051c14aebe371e4e3919dc65ab7c3f
Sharibo <AlexejKrawez@gmail.com>
2023-02-07 21:34:36 +0300

    initial commit

---
+++ src/com/gmail/alexejkrawez/RefactorTest.java
@@ -0,0 +1,47 @@
+package com.gmail.alexejkrawez;
+
+public class RefactorTest {
+//    public void someMethod() {
+//        System.out.println("do Some usual");
+//        System.out.println("do Some usual1");
+//        RefCreator.printOperativeRef();
+//        System.out.println("do Some usual2");
+//        System.out.println("do Some usual3");
+//    }
+//
+//    public void someElseMethod() {
+//        System.out.println("do Some usual");
+//        System.out.println("do Some usual1");
+//        RefCreator.printHistoricalRef();
+//        System.out.println("do Some usual2");
+//        System.out.println("do Some usual3");
+//    }
+
+    public static void firstMethod(boolean first) {
+        System.out.println("do Some usual");
+        System.out.println("do Some usual1");
+        if (first) {
+            RefCreator.printOperativeRef();
+        } else {
+            RefCreator.printHistoricalRef();
+        }
+        System.out.println("do Some usual2");
+        System.out.println("do Some usual3");
+    }
+
+    static class RefCreator {
+        static void printOperativeRef() {
+            System.out.println("operative ref");
+        }
+
+        static void printHistoricalRef() {
+            System.out.println("historical ref");
+        }
+    }
+
+    public static void main(String[] args) {
+//        RefactorTest refactorTest = new RefactorTest();
+        RefactorTest.firstMethod(true);
+        RefactorTest.firstMethod(false);
+    }
+}
